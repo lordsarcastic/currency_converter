d2d2dc42560e19ce94dcd2bfc4f5e70af5116faf
Lord-sarcastic <adeoti.15.jude@gmail.com>
2022-11-16 17:03:06 +0100

    Feature: Made app support origins

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.routes import router
 from backend.settings import settings
@@ -12,6 +13,13 @@ app = FastAPI(title=settings.APP_TITLE)
 
 app.include_router(router, prefix="/api/v1")
 
+app.add_middleware(
+    CORSMiddleware,
+    origins=settings.ALLOWED_CLIENTS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
